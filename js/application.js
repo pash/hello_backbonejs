82020100e2d2854d6d3341271fcfc710cc4b8a36
pash <patrickphilipr.sanchez@gmail.com>
2014-09-29 14:04:34 -0700

    simple backbonejs

---
+++ js/application.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var Appointment = Backbone.Model.extend({});
+  var appointment = new Appointment();
+  appointment.set('title', 'My knee hurts');
+
+  var AppointmentView = Backbone.View.extend({
+    render: function(){
+      $(this.el).html('<li>' + this.model.get('title') + '</li>');
+    }
+  });
+  var appointmentView = new AppointmentView({model: appointment});
+
+  appointmentView.render(); 
+  $('#app').html(appointmentView.el);
+});
